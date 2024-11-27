3ee616aa7edcbc24872889d163d1db0967b33930
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2024-11-27 16:48:28 +0100

    kjørte
    kjørte programmet

--- api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20ba3ff682d76a6d96f714abb316f2fb27db6fa7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad13169e84ed87cf2986368aa093325f7d9a5150")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
