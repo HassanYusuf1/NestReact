e08c6848aed425bb49996e0356ecf1e174ed1361
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2024-11-27 15:29:25 +0100

    Endring
    endring

--- api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ee721ea953c237aa7adc7487a3ebcd6028ffe89")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9a97f0b29b613ca5c1b87798e93f959b0e2850")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
