ec6a725ee76ab93401474bfec4b69d498ec2b901
mikailb <115622678+mikailb@users.noreply.github.com>
2024-11-28 11:06:57 +0100

    d

--- api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44c402bf2ed5978e786ca595d6ca2c36b21c32a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bb4679138fa737fb166b36882f7842f39faf44c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
