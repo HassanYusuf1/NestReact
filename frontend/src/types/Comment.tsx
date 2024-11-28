7b719952a4ec3276c388da8a7eec1fb46180b07e
Andyyxd1 <114660172+Andyyxd1@users.noreply.github.com>
2024-11-28 11:16:00 +0100

    comment.tsx

---
+++ frontend/src/types/Comment.tsx
@@ -0,0 +1,8 @@
+export interface Comment {
+    commentId: number; // Unique identifier for the comment
+    noteId?: number;
+    pictureId?: number;
+    userName: string; // Username of the commenter
+    Content: string; // Text of the comment
+    uploadDate: string; // Timestamp of the comment (ISO 8601 format preferred)
+  }  
