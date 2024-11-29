6c981243647549a5f2da7519a99d6bdcbad32046
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2024-11-29 12:09:28 +0100

    Endring
    endring

--- api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+21af7cb075cf98c503dcf5d29794e90c141d13db")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4470a805936533e27f8577370387b6aba72063b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
