d14a58a30ba7b2e9957c21d6a9a2489385c7266e
mikailb <115622678+mikailb@users.noreply.github.com>
2024-11-30 11:18:57 +0100

    css

--- api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -10,10 +10,11 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fb586813ece6d5c77999e94ac31bb3d41ff125a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72bf9070c0a477513159e5ed367c1483636ea671")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
