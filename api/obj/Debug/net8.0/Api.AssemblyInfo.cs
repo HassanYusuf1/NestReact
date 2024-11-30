764ad8803c3e100c10b7604dc725d92430e83f80
HassanYusuf1 <112115781+HassanYusuf1@users.noreply.github.com>
2024-11-30 23:31:03 +0100

    Endring
    Endring

--- api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+871756816383e294cefbc04b2933e8158e8988d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7793f193364112b04c905c0f62454b559ce96855")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
