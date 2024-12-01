c5aa09fe5777a458171d2ce92b5dcdd5e13892a0
Andyyxd1 <114660172+Andyyxd1@users.noreply.github.com>
2024-12-01 14:46:43 +0100

    ok

--- api/obj/Debug/net8.0/Api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac3c8eb263599277070b81550231c2cef5eb6c3c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56660c5a0f93bfb137156c05837756b143e69f83")]
 [assembly: System.Reflection.AssemblyProductAttribute("Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
